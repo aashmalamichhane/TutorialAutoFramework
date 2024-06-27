29fe8b4b4604d074f4ac9259ffb424cb16ad1c41
Aashma Lamichhane <lamichhane.aashma01@gmail.com>
2024-06-27 21:17:57 +0545

    Login cases

--- src/test/java/tutorialninjatestcases/Login.java
+++ src/test/java/tutorialninjatestcases/Login.java
@@ -2,6 +2,7 @@ package tutorialninjatestcases;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.Test;
@@ -24,7 +25,25 @@ public class Login {
     driver.findElement(By.xpath("//input[@value='Login']")).click();
     Assert.assertTrue(driver.findElement(By.linkText("Change your password")).isDisplayed());
     driver.quit();
+    }
 
+    @Test
+    public void verifyLoginwithInvalidCredentials()
+    {
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(10));
+        driver.get("https://tutorialsninja.com/demo/");
+        driver.findElement(By.xpath("//span[normalize-space()='My Account']")).click();
+        driver.findElement(By.linkText("Login")).click();
+        driver.findElement(By.xpath("//input[@id='input-email']")).sendKeys("aashmdda.info@gmail.com");
+        driver.findElement(By.xpath("//input[@id='input-password']")).sendKeys("Test@123");
+        driver.findElement(By.xpath("//input[@value='Login']")).click();
+
+        String actWarningmsg = driver.findElement(By.xpath("//div[@class='alert alert-danger alert-dismissible']")).getText();
+        String expWarningmsg ="Warning: No match for E-Mail Address and/or Password.";
+        Assert.assertEquals(actWarningmsg,expWarningmsg, "Expected message is not as actual message");
 
     }
 }
