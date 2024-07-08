c56717c228ea8949cb63a3709b4c5ab956cf657e
Aashma lamichhane <aashma.lamichhane@tekvortex.com>
2024-07-08 11:45:34 +0545

    otp VERIFICATION

---
+++ src/test/java/testcases/OTPverification.java
@@ -0,0 +1,67 @@
+package testcases;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import com.mailosaur.MailosaurClient;
+import com.mailosaur.MailosaurException;
+import com.mailosaur.models.*;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.io.IOException;
+import java.time.Duration;
+
+import static org.testng.Assert.assertEquals;
+import static org.testng.AssertJUnit.assertNotNull;
+
+public class OTPverification {
+
+             public String getOTP() throws IOException, MailosaurException {
+                // Available in the API tab of a server
+                String apiKey = "EA2gy6eVTQe1ow9FUbn8hG0ZuGryqx8t";
+                String serverId = "ryy9gtfm";
+                String serverDomain = "ryy9gtfm.mailosaur.net";
+
+                MailosaurClient mailosaur = new MailosaurClient(apiKey);
+                MessageSearchParams params = new MessageSearchParams();
+                params.withServer(serverId);
+                SearchCriteria criteria = new SearchCriteria();
+                criteria.withSentTo("asdas@" + serverDomain);
+
+                Message message = mailosaur.messages().get(params, criteria);
+                System.out.println(message.subject());
+                System.out.println(message.cc());
+                System.out.println(message.to());
+                //body content
+                System.out.println(message.text().body());
+
+                // How many codes?
+                System.out.println(message.text().codes().size()); // 2
+                Code firstCode = message.text().codes().get(0);
+
+
+                String otpCode = firstCode.value(); // "456812"
+                assertNotNull(message);
+                assertEquals("Sign in to VB Express", message.subject());
+
+                return otpCode;
+            }
+
+            @Test
+            public void OTPTest() throws MailosaurException, IOException {
+                WebDriver driver = new ChromeDriver();
+                driver.manage().window().maximize();
+                driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+                driver.get("https://vbexpress.stage.veribom.com/");
+                String currPath = System.getProperty("user.dir");
+                String filepath = currPath + "\\TestFile.gz";
+                driver.findElement(By.xpath("//input[@type=\"file\"]")).sendKeys(filepath);
+                driver.findElement(By.xpath("//button[normalize-space()=\"Generate Report\"]")).click();
+                driver.findElement(By.xpath("//input[@id=\"Email\"]")).sendKeys("asdas@ryy9gtfm.mailosaur.net");
+                driver.findElement(By.xpath("//button[normalize-space()=\"Get OTP\"]")).click();
+                driver.findElement(By.xpath("//input[@id=\"OTP\"]")).sendKeys(getOTP());
+                driver.findElement(By.xpath("//button[@type=\"submit\"]")).click();
+
+        }
+}
